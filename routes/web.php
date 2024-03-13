80a12e783f316d902ca01e0f1eefd2862ad4789f
NaufalHafizhAthhar <hafizhathhar30@gmail.com>
2024-03-13 15:31:49 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HaiController;
 
 
-Route::get('/postechno/',[HaiController::class,'index']);
+Route::get('/postechno/{nik}/{nama}/cek',[HaiController::class,'index']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('dashboard', [AuthController ::class, 'dashboard'])->name('dashboard');
+Route::get('login', [AuthController::class, 'index'])->name('login');
+//Route::post('post-login', [AuthController::class, 'postLogin'])->name('login.post'); 
+Route::get('registration', [AuthController::class, 'registration'])->name('register');
+//Route::post('post-registration', [AuthController::class, 'postRegistration'])->name('register.post'); 
+//Route::get('logout', [AuthController::class, 'logout'])->name('logout');
